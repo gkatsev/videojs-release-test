6939a3be00536fad813c86778e43a67701a35a10
Gary Katsevman <git@gkatsev.com>
2020-01-08 16:17:19 -0500

    chore: add semantic-release github action

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  dryRun: true,
+  plugins: [
+    ['@semantic-release/commit-analyzer', {
+      preset: 'videojs'
+    }],
+    ['@semantic-release/release-notes-generator', {
+      preset: 'videojs'
+    }],
+    ['@semantic-release/npm', {
+      tag: 'next'
+    }],
+    ['@semantic-release/github', {
+      assets: 'dist/video-js*.zip'
+    }],
+    ['@semantic-release/git', {
+      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+      message: '${nextRelease.version}'
+    }]
+  ]
+};
